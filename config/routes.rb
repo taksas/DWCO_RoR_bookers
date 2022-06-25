5a3942669aebfe0ec0c0817dc9216b0da8a15cd6
taksas <takuminakayama4869@gmail.com>
2022-06-25 04:35:49 +0000

    ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   get '/' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
