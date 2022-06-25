8f25b208def8dad18e3c429e87733f00658adcf2
taksas <takuminakayama4869@gmail.com>
2022-06-25 08:08:39 +0000

    FINISHED-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,27 +4,48 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), flash: { notice: 'Book was successfully updated.' }
+    else
+      render :edit
+    end
+  end
+
+
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/'
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), flash: { notice: 'Book was successfully created.' }
+    else
+      render :index
+    end
   end
 
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
 
   private
   # ストロングパラメータ
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   get '/' => 'homes#top'
   get '/books' => 'books#index'
   post '/books' => 'books#create'
-  resources :lists
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
