717e320f45c25569f12cab5b12fb1ad09308364a
taksas <takuminakayama4869@gmail.com>
2022-06-25 05:12:12 +0000

    トップページをいい感じに＆新規登録部分作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,28 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/'
+  end
+
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
+  get '/books' => 'books#index'
+  post '/books' => 'books#create'
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
