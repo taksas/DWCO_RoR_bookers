075e01dbbaa8bb010f0de2b2838723d213d61098
taksas <takuminakayama4869@gmail.com>
2022-06-25 04:32:34 +0000

    アプリケーションに必要なファイルの準備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "ddf7a2f5e2f84ecc89aaa7e313d436fd.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
